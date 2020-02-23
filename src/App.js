f3bdad6030338572213dfb8939969b1276720e51
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-02-23 14:37:02 +0200

    feature(Search user): add Search user
    - add search form
    - add display found users in cards

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './components/pages/Login';
 import Signup from './components/pages/Signup';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layouts/Alert';
 
 class App extends Component {
   state = {
@@ -19,14 +21,27 @@ class App extends Component {
     &client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
     this.setState({ users: res.data });
   }
+  searchUsers = async text => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items });
+  }
+  setAlert = message => {
+    this.setState({ alert: { message } });
+    setTimeout(() => this.setState({ alert: null }), 2000);
+  }
   render() {
     return (
       <div>
         <Navbar />
         <Router>
+        <Alert alert={this.state.alert} />
           <Switch>
           <Route exact path='/' render={props => (
-                <Fragment>
+              <Fragment>
+                <Search
+                      searchUsers={this.searchUsers}
+                      setAlert={this.setAlert}
+                    />
                     <Users users={this.state.users} />
                 </Fragment>
               )} />
